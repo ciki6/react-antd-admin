78c821fb9e243e200e7639de679fc0865cdeeb3b
ciki6 <18694942447@163.com>
2020-09-14 11:53:20 +0800

    add all

---
+++ craco.config.js
@@ -0,0 +1,34 @@
+const CracoLessPlugin = require('craco-less');
+const path = require('path')
+
+const pathResolve = pathUrl => path.join(__dirname, pathUrl)
+
+module.exports = {
+    webpack: {
+        //配置路径别名
+        alias: {
+            '@': pathResolve('src'),
+        }
+    },
+    babel: {
+        //配置antd按需加载
+        plugins: [
+            ['import', { libraryName: 'antd', style: true }],
+            ['@babel/plugin-proposal-decorators', { legacy: true }]
+        ]
+    },
+    plugins: [
+        {
+            plugin: CracoLessPlugin,
+            options: {
+                //配置less-loader
+                lessLoaderOptions: {
+                    lessOptions: {
+                        modifyVars: {'@primary-color': '#1DA57A'},
+                        javascriptEnabled: true,
+                    },
+                },
+            },
+        },
+    ],
+};
---
+++ src/views/login/index.jsx
@@ -0,0 +1,102 @@
+import React, {useState} from "react";
+import {Redirect} from "react-router-dom";
+import {Form, Icon, Input, Button, message, Spin} from "antd";
+import {connect} from "react-redux";
+import DocumentTitle from "react-document-title";
+import "./index.less";
+import {login, getUserInfo} from "@/store/actions";
+
+const Login = (props) => {
+    const {token, login, getUserInfo} = props;
+
+    const [loading, setLoading] = useState(false);
+    const [form] = Form.useForm();
+
+    const handleLogin = (username, password) => {
+        // 登录完成后 发送请求 调用接口获取用户信息
+        setLoading(true);
+        login(username, password)
+            .then((data) => {
+                message.success("登录成功");
+                handleUserInfo(data.token);
+            })
+            .catch((error) => {
+                setLoading(false);
+                message.error(error);
+            });
+    };
+
+    // 获取用户信息
+    const handleUserInfo = (token) => {
+        getUserInfo(token)
+            .then((data) => {})
+            .catch((error) => {
+                message.error(error);
+            });
+    };
+
+    const handleFinish = (values) => {
+        const {username, password} = values;
+        handleLogin(username, password);
+    };
+
+    if (token) {
+        return <Redirect to="/dashboard"/>;
+    }
+    return (
+        <DocumentTitle title={"用户登录"}>
+            <div className="login-container">
+                <Form onFinish={handleFinish} className="content"
+                      initialValues={{username: 'admin', password: '123456'}}>
+                    <div className="title">
+                        <h2>用户登录</h2>
+                    </div>
+                    <Spin spinning={loading} tip="登录中...">
+                        <Form.Item name="username" rules={[
+                            {
+                                required: true,
+                                whitespace: true,
+                                message: "请输入用户名",
+                            },
+                        ]}>
+                            <Input
+                                prefix={
+                                    <Icon type="user" style={{color: "rgba(0,0,0,.25)"}}/>
+                                }
+                                placeholder="用户名"
+                            />
+                        </Form.Item>
+                        <Form.Item name="password" rules={[
+                            {
+                                required: true,
+                                whitespace: true,
+                                message: "请输入密码",
+                            },
+                        ]}>
+                            <Input
+                                prefix={
+                                    <Icon type="lock" style={{color: "rgba(0,0,0,.25)"}}/>
+                                }
+                                type="password"
+                                placeholder="密码"
+                            />
+                        </Form.Item>
+                        <Form.Item>
+                            <Button
+                                type="primary"
+                                htmlType="submit"
+                                className="login-form-button"
+                            >
+                                登录
+                            </Button>
+                        </Form.Item>
+                    </Spin>
+                </Form>
+            </div>
+        </DocumentTitle>
+    );
+};
+
+export default connect((state) => state.user, {login, getUserInfo})(
+    Login
+);
